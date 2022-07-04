03062e0be64eaa36b5348a769f929479f9bbd20a
Сморкалов Алексей <leshasmlesha@gmail.com>
2022-07-04 14:54:22 +0000

    Поправил наименование

--- src/Pages/Data/Data.module.ts
+++ src/Pages/Data/Data.module.ts
@@ -2,7 +2,7 @@ import { SequelizeModule } from "@contact/nestjs-sequelize";
 import { Module } from "@nestjs/common";
 import { Barcode } from "src/Database/Local.database/models/Barcode.model";
 import { Doc } from "src/Database/Local.database/models/Doc.model";
-import { DataController } from "./Data.Controller";
+import { DataController } from "./Data.controller";
 import { DataService } from "./Data.service";
 
 @Module({
