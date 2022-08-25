b91aae15baebf226d08b09f0f25cb96298ba0619
Ivanova Maria <ivanova_ms@outlook.com>
2022-08-25 15:15:04 +0300

    добавлены данные таблицы barcode

--- src/Pages/GetDocs/GetDocs.module.ts
+++ src/Pages/GetDocs/GetDocs.module.ts
@@ -1,14 +1,26 @@
 import { ConstValue, DocAttach } from '@contact/models';
 import { SequelizeModule } from '@contact/nestjs-sequelize';
 import { Module } from '@nestjs/common';
+import { Barcode } from 'src/Database/Local.database/models/Barcode.model';
+import { Depart } from 'src/Database/Local.database/models/Depart.model';
 import { Doc } from 'src/Database/Local.database/models/Doc.model';
+import { Transmit } from 'src/Database/Local.database/models/Transmit.model';
+import { User } from 'src/Database/Local.database/models/User.model';
 import { SmbModule } from 'src/Modules/Smb/Smb.module';
 import { GetDocsController } from './GetDocs.controller';
 import { GetDocsService } from './GetDocs.service';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Doc, DocAttach, ConstValue]),
+    SequelizeModule.forFeature([
+      Doc,
+      DocAttach,
+      ConstValue,
+      Transmit,
+      Barcode,
+      User,
+      Depart,
+    ]),
     SmbModule,
   ],
   controllers: [GetDocsController],
--- src/Pages/GetDocs/GetDocs.service.ts
+++ src/Pages/GetDocs/GetDocs.service.ts
@@ -7,10 +7,20 @@ import Filter from 'src/utils/Filter';
 import TableDocsColumns from 'src/utils/Columns/TableDocs';
 import { GridFilterModel, GridSortModel } from '@mui/x-data-grid-premium';
 import Sort from 'src/utils/Sort';
+import { Transmit } from 'src/Database/Local.database/models/Transmit.model';
+import { Barcode } from 'src/Database/Local.database/models/Barcode.model';
+import { User } from 'src/Database/Local.database/models/User.model';
+import { Depart } from 'src/Database/Local.database/models/Depart.model';
 
 @Injectable()
 export class GetDocsService {
-  constructor(@InjectModel(Doc) private modelDoc: typeof Doc) {}
+  constructor(
+    @InjectModel(Doc) private modelDoc: typeof Doc,
+    @InjectModel(Transmit) private modelTransmit: typeof Transmit,
+    @InjectModel(Barcode) private modelBarcode: typeof Barcode,
+    @InjectModel(User) private modelUser: typeof User,
+    @InjectModel(Depart) private modelDepart: typeof Depart,
+  ) {}
 
   async find(body: GetDocsInput) {
     const columns = TableDocsColumns();
@@ -23,6 +33,23 @@ export class GetDocsService {
     options.offset = offset;
     options.where = filters(body.filterModel);
     options.order = sorts(body.sortModel);
+    options.include = [
+      {
+        model: this.modelBarcode,
+        required: false,
+        include: [
+          {
+            model: this.modelTransmit,
+            required: false,
+            where: { active: true },
+          },
+          { model: this.modelUser, required: false },
+          { model: this.modelDepart, required: false },
+          'User',
+          'Depart',
+        ],
+      },
+    ];
     return await this.modelDoc.findAndCountAll(options);
   }
 }
