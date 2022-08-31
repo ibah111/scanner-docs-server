28ec8a138dedab7a51a62d02eeaf069d77f66146
Ivanova Maria <ivanova_ms@outlook.com>
2022-08-31 17:14:32 +0300

    изменен headerName в таблице

--- src/utils/Columns/TableDocs.ts
+++ src/utils/Columns/TableDocs.ts
@@ -2,26 +2,26 @@ import { GridColumns } from '@mui/x-data-grid-premium';
 
 const TableDocsColumns = (): GridColumns => [
   { field: 'Doc.id', headerName: 'ID', type: 'number' },
-  { field: 'Doc.title', headerName: 'title', type: 'string' },
-  { field: 'contact_doc_id', headerName: 'contact_doc', type: 'number' },
+  { field: 'Doc.title', headerName: 'Название документа', type: 'string' },
+  { field: 'contact_doc_id', headerName: 'Номер документа', type: 'number' },
   { field: 'mail_id', headerName: 'mail', type: 'number' },
   { field: 'law_act_id', headerName: 'law_act', type: 'number' },
   { field: 'law_exec_id', headerName: 'law_exec', type: 'number' },
   { field: 'date', headerName: 'date', type: 'date' },
   {
     field: 'Barcode.Depart.title',
-    headerName: 'titleDepart',
+    headerName: 'Подразделение',
     type: 'string',
   },
-  { field: 'Barcode.User.f', headerName: 'fio', type: 'string' },
+  { field: 'Barcode.User.f', headerName: 'Текущий держатель', type: 'string' },
   // {
   //   field: 'Barcode.Transmits.date_send',
-  //   headerName: 'date_send',
+  //   headerName: 'Дата отправки в банк/ОСП',
   //   type: 'date',
   // },
   // {
   //   field: 'Barcode.Transmits',
-  //   headerName: 'where_send',
+  //   headerName: 'Куда отправлено',
   //   type: 'string',
   // },
 ];
