d6084ec015b092c99a5970809b6699bea18109fa
Ivanova Maria <ivanova_ms@outlook.com>
2022-09-28 11:12:05 +0300

    исправлена ошибка при добавлении пользователя

--- src/Pages/Role/Role.service.ts
+++ src/Pages/Role/Role.service.ts
@@ -1,8 +1,10 @@
 import { InjectModel } from '@contact/nestjs-sequelize';
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { Role } from 'src/Database/Local.database/models/Role.model';
 import { User } from 'src/Database/Local.database/models/User.model';
 import { User_Role } from 'src/Database/Local.database/models/User_Role.model';
+import server from 'src/utils/server';
 import { RoleInputAddRole, RoleInputAddUser } from './Role.input';
 
 @Injectable()
@@ -30,7 +32,32 @@ export class RoleService {
     return true;
   }
   async addUser(body: RoleInputAddUser) {
-    await this.modelUser.create({ ...body });
+    const data_user = await this.modelUser;
+    const res = (
+      await axios({
+        method: 'post',
+        url: `${server()}/scripts/structure.php`,
+        data: { action: 'user', token: server('token') },
+      })
+    ).data;
+    const data = res?.result;
+    const tmp = {};
+    const users = await this.modelUser.findAll();
+    const user_ids = users.map((user) => user.bitrix_id);
+
+    for (const user of data) {
+      if (user.login == body.login && !user_ids.includes(Number(user.id))) {
+        data_user.create({
+          bitrix_id: user.id,
+          f: user.f,
+          i: user.i,
+          o: user.o,
+          login: user.login,
+          depart: Number(tmp[user.depart]),
+          position: user.position,
+        });
+      }
+    }
     return true;
   }
   async removeUser(id: number) {
