594df0fa25c49dc3756b17e37a918ccbce462183
Ibah <starlord77@mail.ru>
2023-11-14 16:47:23 +0300

    feat(seeds): сиды ролей

---
+++ src/Database/Local.database/seeds/001-add-roles.ts
@@ -0,0 +1,15 @@
+import { QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.sequelize.models.Role.bulkCreate(
+        [
+          { id: 1, name: 'admin', title: 'Администратор' },
+          { id: 2, name: 'moderator', title: 'Модератор' },
+        ],
+        { transaction: t },
+      ),
+    ]),
+  );
