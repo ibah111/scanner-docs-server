9d329781cac91a928601043e84aab48bec340d1a
Ibah <starlord77@mail.ru>
2023-12-08 16:29:52 +0300

    feat(openHistory): преобразовал тело инпута в параметр

--- src/Pages/OpenHistory/OpenHistory.controller.ts
+++ src/Pages/OpenHistory/OpenHistory.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { OpenHistoryService } from './OpenHistory.service';
 import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
 import { CanGuard } from '../../Modules/CASL/Can.guard';
@@ -13,8 +20,8 @@ export class OpenHistoryController {
   constructor(private openHistoryService: OpenHistoryService) {}
 
   @HttpCode(200)
-  @Post()
-  async openHistory(@Body() input: { code: number }) {
-    return await this.openHistoryService.openHistory(input);
+  @Get(':code')
+  async openHistory(@Param('code', ParseIntPipe) code: number) {
+    return await this.openHistoryService.openHistory(code);
   }
 }
