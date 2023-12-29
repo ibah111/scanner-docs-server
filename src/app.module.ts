0847fd6d29b75c04e75e5ffacf98933623040e67
Ibah <starlord77@mail.ru>
2023-12-29 14:04:23 +0300

    feat(release-prepare): подготовка к релизу, разделение на dev / prod базы

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CommandModule } from './Command/Command.module';
     RoleModule,
     ConfigModule.forRoot({
       load: [databaseConfig],
+      envFilePath: process.env.NODE_ENV === 'prod' ? '.env.prod' : '.env.dev',
       isGlobal: true,
     }),
     CommandModule,
