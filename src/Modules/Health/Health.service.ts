a66151678b7d77c2af0b0b7e6255b4d7cb33c10f
Ibah <starlord77@mail.ru>
2024-01-09 15:37:06 +0300

    chore: убрал проверку локальной базы

--- src/Modules/Health/Health.service.ts
+++ src/Modules/Health/Health.service.ts
@@ -15,7 +15,6 @@ export class HealthService {
   ) {}
   check() {
     return this.health.check([
-      () => this.db.pingCheck('local-database'),
       () => this.db.pingCheck('contact-database', { connection: 'contact' }),
       () => this.http.pingCheck('bitrix', 'https://chat.nbkfinance.ru'),
       () => this.smb.check('smb'),
