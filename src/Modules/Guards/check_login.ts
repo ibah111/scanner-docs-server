219a0d95ad3a5f40cdb754d171705c8c821f8536
Ibah <starlord77@mail.ru>
2024-01-17 19:09:56 +0300

    chore: добавил данных для разработчика

--- src/Modules/Guards/check_login.ts
+++ src/Modules/Guards/check_login.ts
@@ -5,33 +5,54 @@ import client from 'src/utils/client';
 import bitrix from 'src/utils/bitrix';
 
 export const checkLogin = async (token: string) => {
-  // Тестируем систему................................
-  console.log('check login: ', token);
-  if (process.env.NODE_ENV === 'dev')
-    return {
-      login_result: true,
-      login: 'baledin@zakon43.ru',
-    } as unknown as AuthUserSuccess;
+  /**
+   * тестовый ответ разработчика для пропуска в приложение
+   */
+  const dev_response = {
+    output: 'Вы вошли',
+    id: '4018',
+    login_result: true,
+    login: 'baledin@zakon43.ru',
+    birthdate: '18.10.2001',
+    department: 'Группа разработки IT',
+    position: 'Младший разработчик',
+    firstname: 'Иван',
+    secondname: 'Балезин',
+    thirdname: '',
+  };
 
-  try {
-    const result = await axios.get<AuthUser<boolean>>(
-      bitrix('oauth') + '/oauth/login',
-      { headers: { token } },
-    );
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const dev_condition = () => {
+    return process.env.NODE_ENV === 'dev';
+  };
+  /** */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const problem_condition = () => {
+    return client('demo');
+  };
 
-    if (result.data === undefined || result.data === null) {
-      return false;
-    } else {
-      if (result.data.login_result === true) {
-        return result.data as AuthUserSuccess;
+  if (problem_condition()) {
+    return dev_response as unknown as AuthUserSuccess;
+  } else {
+    try {
+      const url = bitrix('oauth') + '/oauth/login';
+      const result = await axios.get<AuthUser<boolean>>(url, {
+        headers: { token },
+      });
+      if (result.data === undefined || result.data === null) {
+        return false;
       } else {
-        return result.data as AuthUserError;
+        if (result.data.login_result === true) {
+          return result.data as AuthUserSuccess;
+        } else {
+          return result.data as AuthUserError;
+        }
       }
+    } catch (err) {
+      throw new UnauthorizedException({
+        message: 'Не удалось прочитать токен доступа',
+        code: 'error_token',
+      });
     }
-  } catch (err) {
-    throw new UnauthorizedException({
-      message: 'Не удалось прочитать токен доступа',
-      code: 'error_token',
-    });
   }
 };
