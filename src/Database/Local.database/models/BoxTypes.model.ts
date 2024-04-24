a3deb1edbe61410d116d258f7041373adf861aca
Ibah <starlord77@mail.ru>
2024-04-24 15:57:30 +0300

    feat(models): added box types model

---
+++ src/Database/Local.database/models/BoxTypes.model.ts
@@ -0,0 +1,41 @@
+import type {
+  CreationOptional,
+  InferAttributes,
+  InferCreationAttributes,
+  NonAttribute,
+} from '@sql-tools/sequelize';
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  DataType,
+  HasMany,
+  Model,
+  PrimaryKey,
+  Table,
+  Unique,
+} from '@sql-tools/sequelize-typescript';
+import { Barcode } from './Barcode.model';
+
+@Table({ tableName: 'BoxTypes' })
+export class BoxTypes extends Model<
+  InferAttributes<BoxTypes>,
+  InferCreationAttributes<BoxTypes>
+> {
+  @AutoIncrement
+  @PrimaryKey
+  @Column(DataType.INTEGER)
+  id: CreationOptional<number>;
+
+  @AllowNull(false)
+  @Unique
+  @Column(DataType.STRING)
+  title: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  who_added_type: string;
+
+  @HasMany(() => Barcode)
+  Barcodes: NonAttribute<Barcode[]>;
+}
