842f2887e11e3bf4fdae206f87dfce25d4181ee3
Ibah <starlord77@mail.ru>
2024-04-25 15:48:13 +0300

    feat(api): added api description for DoMail api create method

--- src/Pages/Create/Create.controller.ts
+++ src/Pages/Create/Create.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateInput } from './Create.input';
 import { CreateService } from './Create.service';
 import { Auth, AuthGuard, AuthResult } from 'src/Modules/Guards/auth.guard';
-import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBasicAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CanGuard } from '../../Modules/CASL/Can.guard';
 @ApiTags('Create')
 @Controller('create')
@@ -11,6 +11,11 @@ import { CanGuard } from '../../Modules/CASL/Can.guard';
 @ApiBasicAuth()
 export class CreateController {
   constructor(private createService: CreateService) {}
+  @ApiOperation({
+    summary: 'Это апи вызывается при добавлении документа в DO-Mail-e',
+    description:
+      'В DocAdder при добавлении указываются данные для почты. Если при добавлении был распевчатан баркод, то при получении сигнала, почта общается с данной АПИ, и данная АПИ создает баркод на отслеживание',
+  })
   @Post()
   async create(@Body() body: CreateInput, @Auth() auth: AuthResult) {
     return await this.createService.create(body, auth);
