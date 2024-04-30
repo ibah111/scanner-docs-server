dcfb67a7a46ed779ea5a0615694fc1d9bc4ed270
Ibah <starlord77@mail.ru>
2024-04-30 18:32:24 +0300

    feat(migrations): dropping barcode types table

---
+++ src/Database/Local.database/migrations/021-drop-barcode-types-table.ts
@@ -0,0 +1,34 @@
+import { DataTypes, QueryInterface } from '@sql-tools/sequelize';
+import { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([context.dropTable('Barcodes', { transaction: t })]),
+  );
+
+export const down: MigrationFn<QueryInterface> = ({ context }) =>
+  context.sequelize.transaction((t) =>
+    Promise.all([
+      context.createTable(
+        'BarcodeTypes',
+        {
+          id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+            unique: true,
+          },
+          title: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+        },
+        { transaction: t },
+      ),
+    ]),
+  );
