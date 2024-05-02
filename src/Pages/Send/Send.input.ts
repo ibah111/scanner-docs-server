0dc090702f199d77b17554d725879849e72c7fb0
Ibah <starlord77@mail.ru>
2024-05-02 11:18:11 +0300

    feat(api): api scanner send input

--- src/Pages/Send/Send.input.ts
+++ src/Pages/Send/Send.input.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class SendInput {
+  @ApiProperty({
+    description: 'id???',
+    default: 1,
+  })
   @IsNotEmpty()
   @IsNumber()
   id: number;
+  @ApiProperty({
+    description: 'Дата отправления',
+    default: new Date(),
+  })
   @Type(() => Date)
   @IsDate()
   @IsNotEmpty()
   DateSend: Date;
+  @ApiProperty({
+    description: 'Куда было отправлено',
+    default: 'Суд в который отправляю документ',
+  })
   @IsString()
   @IsNotEmpty()
   WhereSend: string;
