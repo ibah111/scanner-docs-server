4b1229cedef925acc82ab8162693f4f73254b620
Ibah <starlord77@mail.ru>
2024-06-13 14:43:54 +0300

    feat: change to if

--- src/Pages/GetDocs/GetDocs.service.ts
+++ src/Pages/GetDocs/GetDocs.service.ts
@@ -105,56 +105,52 @@ export class GetDocsService {
         },
       ];
       const type = doc.doc_type;
-      console.log(type);
-      switch (type) {
-        case 1: {
-          /**
-           * Поиск по law_act
-           */
-          const docLawAct = await this.modelLawAct.findOne({
-            where: {
-              id: doc.law_case_id!,
-            },
-            include,
-          });
-          for (const res of data_result) {
-            if (doc.DocData!.result == res.id) {
-              res.kd = docLawAct!.Debt!.contract;
-              res.reestr = docLawAct!.Portfolio!.name;
-              res.fio_dol =
-                docLawAct!.Person!.f +
-                ' ' +
-                docLawAct!.Person!.i +
-                ' ' +
-                docLawAct!.Person!.o;
-              res.date_post = docLawAct!.Portfolio!.load_dt;
-              await res.save();
-            }
+      if (type === 1) {
+        /**
+         * Поиск по law_act
+         */
+        const docLawAct = await this.modelLawAct.findOne({
+          where: {
+            id: doc.law_case_id!,
+          },
+          include,
+        });
+        for (const res of data_result) {
+          if (doc.DocData!.result == res.id) {
+            res.kd = docLawAct!.Debt!.contract;
+            res.reestr = docLawAct!.Portfolio!.name;
+            res.fio_dol =
+              docLawAct!.Person!.f +
+              ' ' +
+              docLawAct!.Person!.i +
+              ' ' +
+              docLawAct!.Person!.o;
+            res.date_post = docLawAct!.Portfolio!.load_dt;
+            await res.save();
           }
         }
-        case 2: {
-          /**
-           * Поиск по law_exec
-           */
-          const docLawExec = await this.modelLawExec.findOne({
-            where: {
-              id: doc.law_case_id!,
-            },
-            include,
-          });
-          for (const res of data_result) {
-            if (doc.DocData!.result == res.id) {
-              res.kd = docLawExec!.Debt!.contract;
-              res.reestr = docLawExec!.Portfolio!.name;
-              res.fio_dol =
-                docLawExec!.Person!.f +
-                ' ' +
-                docLawExec!.Person!.i +
-                ' ' +
-                docLawExec!.Person!.o;
-              res.date_post = docLawExec!.Portfolio!.load_dt;
-              await res.save();
-            }
+      } else if (type === 2) {
+        /**
+         * Поиск по law_act
+         */
+        const docLawExec = await this.modelLawExec.findOne({
+          where: {
+            id: doc.law_case_id!,
+          },
+          include,
+        });
+        for (const res of data_result) {
+          if (doc.DocData!.result == res.id) {
+            res.kd = docLawExec!.Debt!.contract;
+            res.reestr = docLawExec!.Portfolio!.name;
+            res.fio_dol =
+              docLawExec!.Person!.f +
+              ' ' +
+              docLawExec!.Person!.i +
+              ' ' +
+              docLawExec!.Person!.o;
+            res.date_post = docLawExec!.Portfolio!.load_dt;
+            await res.save();
           }
         }
       }
